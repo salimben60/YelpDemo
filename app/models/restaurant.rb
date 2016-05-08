a6567ba1422ab2476b0a5bb97b13a6b8afdc5f3c
Salim Benchekroun <salimben60@gmail.com>
2016-05-08 19:05:35 +0000

    added restaurants and edited CSS

--- app/models/restaurant.rb
+++ app/models/restaurant.rb
@@ -5,11 +5,11 @@ class Restaurant < ActiveRecord::Base
 
   has_many :reviews
 
-  validates :name, :address, :phone, :website, :image, presence: true
-  validates :phone, format: { with: /\A\(\d{3}\) \d{3}-\d{4}\z/,
-    message: "must be in the format (123) 456-7890" }
-  validates :website, format: { with: /\Ahttps?:\/\/.*\z/,
-    message: "must start with http:// or https://" }
-  validates :address, format: { with: /\A\d+[^,]+,[^,]+, [A-Z]{2} \d{5}\z/,
-    message: "must be in the format 350 Fifth Avenue, New York, NY 10118" }
+  #validates :name, :address, :phone, :website, :image, presence: true
+  #validates :phone, format: { with: /\A\(\d{3}\) \d{3}-\d{4}\z/,
+  #  message: "must be in the format (123) 456-7890" }
+  #validates :website, format: { with: /\Ahttps?:\/\/.*\z/,
+  #  message: "must start with http:// or https://" }
+  #validates :address, format: { with: /\A\d+[^,]+,[^,]+, [A-Z]{2} \d{5}\z/,
+  #  message: "must be in the format 350 Fifth Avenue, New York, NY 10118" }
 end
